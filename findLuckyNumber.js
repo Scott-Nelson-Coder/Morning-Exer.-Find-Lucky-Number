2556a4d2f7951284b233a82293007fbfa3a210ff
Scott Nelson <scottninlv@yahoo.com>
2023-07-19 11:28:21 -0700

    Morning work

---
+++ findLuckyNumber.js
@@ -0,0 +1,9 @@
+
+function luckyNumber(num) {
+    let numbers = [1,2,3,4,5,6,7,8,9,10]
+    let numbersPicked = []
+for ( let i = 0; i < num; i++) {
+    Math.floor(Math.random()) * numbers
+
+ }
+}
